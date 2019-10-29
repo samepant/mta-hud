f2133548860a7f2a121d307316c651565a6aec9f
samepant <sampanter@gmail.com>
2019-10-28 23:29:18 -0400

    build directory fix

--- src/server.js
+++ src/server.js
@@ -46,7 +46,7 @@ app.get("/bus", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
